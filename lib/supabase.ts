4b9ed36db8ee8de2b391adb888a13719b9b3b23b
Vlad <nvlad2410@gmail.com>
2026-04-14 16:19:19 +0300

    add real supabase key

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://kwgddrdykzcspwqfynet.supabase.co";
-const supabaseAnonKey = "PUNE_AICI_CHEIA_TA_PUBLICA_SUPABASE";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt3Z2RkcmR5a3pjc3B3cWZ5bmV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU1ODUxMjUsImV4cCI6MjA5MTE2MTEyNX0.VPxI-rK0FUcTyHeyCZevS30DJz9cI2BteQGVcplwpOs";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
